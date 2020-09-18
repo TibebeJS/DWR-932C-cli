3b7a3c21ff40d1ead9d20a9b9634523423b98790
TibebeJs <Tibebes.js@gmail.com>
2020-09-18 16:08:50 -0400

    create a simple cli

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/chromedp/chromedp v0.5.3 // indirect
+	github.com/urfave/cli v1.22.4
 	github.com/urfave/cli/v2 v2.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
 )
 
 func main() {
-	fmt.Println("hello")
+	app := &cli.App{
+		Name:  "DWR-932C CLI tool",
+		Usage: "Helps manage/control the D-Link DWR-932C LTE Router right from the command line",
+		Action: func(c *cli.Context) error {
+			fmt.Println("[+] running CLI tool...")
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
